0979628736dbcc963decfb0df9d849b5004929c1
Lukas Jung <jung@synyx.de>
2020-05-22 19:22:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan')
+const db = require('better-sqlite3')('./db/geocache.db');
+
+let secrets = ["1JA-9NS-3NI","IS8-XZ1-ON9","CM0-9WW-R01","CSK-IV4-03M","4GS-SVT-QND","H9I-DZK-YJN","LRM-6A9-USK","O3H-2TA-T8R","WR3-8KZ-AK7","909-4HN-EUF","K8Q-XV2-6W6","Z9Q-M82-L2Q","4ES-9OF-JRD","2ME-1MZ-1HC","DN0-9VR-1AJ"];
+const app = express();
+
+app.use(morgan('short'));
+app.use(cookieParser());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+  const stmt = db.prepare("Select * from stages");
+  data = stmt.all();
+  
+    return res.render("index", {data:data});
+  });
+
+app.get('/stage', function (req,res){
+  
+    stageNumber = req.query.n;
+    secret = req.query.secret;  
+    number = secrets.indexOf(secret);
+    if(number==-1){
+      solved = false;
+    }
+    solved = true;
+
+    return res.render("stage", { stageNumber: stageNumber, solved: solved });
+
+});
+
+
+app.get('/secret', function (req,res){
+  secret = req.query.secret;
+  number = secrets.indexOf(secret);
+  if(number==-1){
+    return res.redirect("/");
+  }
+  return res.redirect("/stage?n="+number+"&secret="+req.query.secret);
+});
+
+app.listen(3000, function () {
+    console.log("Listening on port 3000");
+  });
